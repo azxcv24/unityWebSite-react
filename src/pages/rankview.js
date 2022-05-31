62acbdea25f8760082499d6039d1947ae3f4243c
azxcv24 <azxcv24@gmail.com>
2022-06-01 02:18:21 +0900

    ADD 게임 랭킹뷰 페이지

---
+++ src/pages/rankview.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import App from '../components/App';
+import Menu from '../components/Menu/Menu';
+import { headData } from '../mock/data';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/main.scss';
+
+export default () => {
+  const { title, lang, description } = headData;
+
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title || 'Gatsby Simplefolio'}</title>
+        <html lang={lang || 'en'} />
+        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+      </Helmet>
+      <Menu />
+    </>
+  );
+};
